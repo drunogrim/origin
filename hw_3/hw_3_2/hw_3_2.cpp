4f8becd0f91666fb3ea7851e35454efd243ddba6
drunogrim <698tma@gmail.com>
2023-08-02 15:17:45 +0300

    Added hw_3_2

---
+++ hw_3/hw_3_2/hw_3_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Counter {
+public:
+	int num;
+	void inc() {
+		num++;
+	}
+	void dec() {
+		num--;
+	}
+
+
+
+};
+
+
+int main()
+{
+
+
+	return 0;
+}
+
