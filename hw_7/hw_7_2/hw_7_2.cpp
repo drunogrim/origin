333a83bc7e460e879f8964debb5c6e998b97e097
drunogrim <698tma@gmail.com>
2023-10-26 16:20:44 +0300

    added hw_7_2

---
+++ hw_7/hw_7_2/hw_7_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define SUB(a, b) (a - (b))
+
+using namespace std;
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	cout << SUB(a, b) << endl;
+	cout << SUB(a, b) * c << endl;
+	cout << SUB(a, b + c) * c << endl;
+	return 0;
+}
